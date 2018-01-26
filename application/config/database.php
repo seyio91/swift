c175446ff89065134c3d670c383eea49b882e271
Seyi Obaweya <seyi@globalmobility.net>
2018-01-26 14:02:35 +0800

    fixed database

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '192.168.56.20',
 	'username' => 'ci_user',
 	'password' => 'cipassword',
 	'database' => 'ci_database',
